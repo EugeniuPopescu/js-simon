9b688e267e789e5d2763c49d6497061dc36b3e28
Eugen <epopescuu@yahoo.it>
2023-11-10 10:37:16 +0100

    perf: add timeout to paragraph and prompt

--- main.js
+++ main.js
@@ -10,26 +10,54 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
+// faccio sparire il paragrafo dopo 10 secondi
+function shatush() {
+    return document.querySelector('#random').style.display = 'none';
+}
+
+// faccio apparire i prompt dopo che tolgo il paragrafo
+function remember() {
+    // ripeto 5 volte il prompt
+    while (userNumbers.length < 5) {
+        let number = Number(prompt(`Inserisci numero ${userNumbers}`));
+        userNumbers.push(number);
+
+    }
+    console.log(`numeri inseriti dall'utente: ${userNumbers}`);
+
+    for (let i = 0; i < randomNumbers.length; i++) {
+        if (randomNumbers[i] !== userNumbers[i]) {
+            console.log(`I numeri sono uguali in posizione: ${randomNumbers[i] === userNumbers[i]}`);
+        }
+    }
+}
 
 /********** calc **********/
 // mi creo l'array con 5 numeri random non ripetuti 
 let randomNumbers = [];
 let userNumbers = [];
+let uguali = false;
 
 while (randomNumbers.length < 5) {
-    numeroRandom = randomNumber(1, 99);
-
+    numeroRandom = randomNumber(1, 9);
+    
     if (!randomNumbers.includes(numeroRandom)) {
         Number(randomNumbers.push(numeroRandom));
     }
-}
+}    
 
+// inserisco i numeri random nel paragrafo 
+document.querySelector('#random').innerHTML = `I tuoi numeri da ricordare sono ${randomNumbers}`;
 console.log(`numeri random: ${randomNumbers}`);
 
 
-while (userNumbers.length < 5) {
-    let number = Number(prompt(`Inserisci numero ${userNumbers}`));
-    userNumbers.push(number);
+// faccio scomparire i numeri random del paragrafo dopo 10 secondi
+// dico solo qual'è il nome della funzione
+setTimeout(shatush, 5 * 1000)
 
-    console.log(number);
-}
+
+// ricchiamo la funzione
+setTimeout(remember, 6 * 1000)
+
+
+
